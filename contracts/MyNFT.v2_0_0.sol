5cb7225530f838f9dc6f5494387d3b0d2d1cd93c
TypingMonkey <panebinese@gmail.com>
2021-09-30 03:40:00 +0900

    wip

--- contracts/MyNFT.v2_0_0.sol
+++ contracts/MyNFT.v2_0_0.sol
@@ -4,16 +4,21 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 
-contract MyNFT is ERC721Upgradeable {
+contract MyNFT_v2_0_0 is ERC721Upgradeable {
     string public greeting;
 
     function initialize() public initializer {
-        __ERC721_init("MyNFT", "MNFT");
-        greeting = "Hello, world!";
+        __ERC721_init("MyNFT_v2_0_0", "MNFT_v2_0_0");
+        greeting = "Hola, mundo!";
         console.log("initializeing a MyNFT with greeting:", greeting);
     }
 
     function greet() public view returns (string memory) {
         return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+        greeting = _greeting;
     }
 }
--- test/MyNFT.ts
+++ test/MyNFT.ts
@@ -1,26 +1,47 @@
 import chai, { expect } from "chai";
-import { Signer } from "ethers";
-import { ethers } from "hardhat";
+import { Contract, Signer } from "ethers";
+import { ethers, upgrades } from "hardhat";
 import { solidity } from "ethereum-waffle";
-import { MyNFT__factory, MyNFT } from "../typechain";
+import { MyNFT, MyNFTV200, MyNFTV200__factory, MyNFT__factory } from "../typechain";
 
 chai.use(solidity);
 
 describe("MyNFT", () => {
   let accounts: Signer[];
-  let mynftContractFactory: MyNFT__factory;
-  let mynft: MyNFT;
+  let mynftContractFactoryV1: MyNFT__factory;
+  let mynftV1: MyNFT;
+  let mynftContractFactoryV2: MyNFTV200__factory;
+  let mynftV2: MyNFTV200;
 
   before(async () => {
     accounts = await ethers.getSigners();
-    mynftContractFactory = <MyNFT__factory>(
+    mynftContractFactoryV1 = <MyNFT__factory>(
       await ethers.getContractFactory("MyNFT")
     );
-    mynft = await mynftContractFactory.deploy();
-    await mynft.deployed();
+    mynftV1 = <MyNFT>await upgrades.deployProxy(mynftContractFactoryV1);
+    await mynftV1.deployed();
+    mynftContractFactoryV2 = <MyNFTV200__factory>(
+      await ethers.getContractFactory("MyNFT_v2_0_0")
+    );
   });
 
   it("Should return the greeting", async () => {
-    expect(await mynft.greet()).to.equal("Hello, world!");
+    expect(await mynftV1.greet()).to.equal("Hello, world!");
   });
+
+  it("Should return the greeting after upgrading", async () => {
+    const mynftV2 = await upgrades.upgradeProxy(
+      mynftV1.address,
+      mynftContractFactoryV2
+    );
+    expect(await mynftV2.greet()).to.equal("Hello, world!");
+  });
+
+  it("Should return the new greeting once it's changed", async () => {
+    const setGreetingTx = await mynftV2.setGreeting("Hola, mundo!");
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
+    expect(await mynftV2.greet()).to.equal("Hola, mundo!");
+  });
+
 });
