834ade777356bfbd2e0f2e271500d25d3a0cf52e
typingmonkey <panebinese@gmail.com>
2021-09-29 19:48:02 +0900

    feat: add a modifier, only owner

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -5,17 +5,30 @@ import "hardhat/console.sol";
 
 contract Greeter {
     string private greeting;
+    address private ownerAddress;
 
     constructor(string memory _greeting) {
         console.log("Deploying a Greeter with greeting:", _greeting);
         greeting = _greeting;
+        ownerAddress = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        console.log(
+            "Checking sender address, owner:",
+            ownerAddress,
+            ", sender:",
+            msg.sender
+        );
+        require(msg.sender == ownerAddress, "You are not the owner.");
+        _;
     }
 
     function greet() public view returns (string memory) {
         return greeting;
     }
 
-    function setGreeting(string memory _greeting) public {
+    function setGreeting(string memory _greeting) public onlyOwner {
         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
         greeting = _greeting;
     }
