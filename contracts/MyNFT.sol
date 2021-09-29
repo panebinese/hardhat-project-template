41a5b448c011dba78555720303f3e8454a376f85
TypingMonkey <panebinese@gmail.com>
2021-09-30 02:11:20 +0900

    feat: upgradable contracts/MyNFT.sol

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 
-contract MyNFT is ERC721 {
-    constructor() ERC721("MyNFT", "MNFT") {}
+contract MyNFT is ERC721Upgradeable {
+    function initialize() public initializer {
+        __ERC721_init("MyNFT", "MNFT");
+    }
 }
