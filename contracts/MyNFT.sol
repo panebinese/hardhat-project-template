87ae6e30a88b53673591c757cb3adf301a51a31e
TypingMonkey <panebinese@gmail.com>
2021-09-30 02:37:42 +0900

    feat(MyNFT): greeting message

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 
 contract MyNFT is ERC721Upgradeable {
+    string public greeting = "Hello, world!";
+
     function initialize() public initializer {
         __ERC721_init("MyNFT", "MNFT");
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
     }
 }
