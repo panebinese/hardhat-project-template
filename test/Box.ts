392fd8427b9eb09e141a4b4b191d508b7fc01592
TypingMonkey <panebinese@gmail.com>
2021-09-30 16:59:33 +0900

    test: add test/Box.ts

--- test/Box.ts
+++ test/Box.ts
@@ -2,7 +2,7 @@ import { ethers, upgrades } from "hardhat";
 import chai, { expect } from "chai";
 import { Signer } from "ethers";
 import { solidity } from "ethereum-waffle";
-import { Box, Box__factory } from "../typechain";
+import { Box, BoxV2, Box__factory, BoxV2__factory } from "../typechain";
 
 chai.use(solidity);
 
@@ -10,6 +10,8 @@ describe("Box", () => {
   let accounts: Signer[];
   let boxContractFactory: Box__factory;
   let box: Box;
+  let boxv2ContractFactory: BoxV2__factory;
+  let boxv2: BoxV2;
 
   before(async () => {
     accounts = await ethers.getSigners();
@@ -18,6 +20,7 @@ describe("Box", () => {
       initializer: "store",
     });
     await box.deployed();
+    boxv2ContractFactory = await ethers.getContractFactory("BoxV2");
   });
 
   it("Must return the value if called retrieve.", async () => {
@@ -30,8 +33,32 @@ describe("Box", () => {
     await expect(tx).to.emit(box, "ValueChanged").withArgs(42);
   });
 
-  it("Must return the value if called retrieve after changed.", async () => {
+  it("Must return the value if called retrieve after store.", async () => {
     const val = await box.retrieve();
     expect(val).to.eq(42);
+  });
+
+  it("Must can upgrade the contract.", async () => {
+    boxv2 = <BoxV2>await upgrades.upgradeProxy(box, boxv2ContractFactory);
+    expect(box.address).to.eq(boxv2.address);
+  });
+
+  it("Must remind the value.", async () => {
+    const valv1 = await box.retrieve();
+    const valv2 = await boxv2.retrieve();
+    expect(valv1).to.eq(42);
+    expect(valv2).to.eq(42);
+  });
+
+  it("Must emit ValueChanged if called increment.", async () => {
+    const tx = boxv2.increment();
+    await expect(tx).to.emit(boxv2, "ValueChanged").withArgs(43);
+  });
+
+  it("Must return the value if called retrieve after increment.", async () => {
+    const valv1 = await box.retrieve();
+    const valv2 = await boxv2.retrieve();
+    expect(valv1).to.eq(43);
+    expect(valv2).to.eq(43);
   });
 });
