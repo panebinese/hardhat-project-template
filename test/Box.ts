31b087c01125a421097dd89ffecaeb7178d194e9
TypingMonkey <panebinese@gmail.com>
2021-09-30 16:43:54 +0900

    test: use upgrade

--- test/Box.ts
+++ test/Box.ts
@@ -1,6 +1,6 @@
+import { ethers, upgrades } from "hardhat";
 import chai, { expect } from "chai";
 import { Signer } from "ethers";
-import { ethers } from "hardhat";
 import { solidity } from "ethereum-waffle";
 import { Box, Box__factory } from "../typechain";
 
@@ -14,8 +14,15 @@ describe("Box", () => {
   before(async () => {
     accounts = await ethers.getSigners();
     boxContractFactory = <Box__factory>await ethers.getContractFactory("Box");
-    box = await boxContractFactory.deploy();
+    box = <Box>await upgrades.deployProxy(boxContractFactory, [0], {
+      initializer: "store",
+    });
     await box.deployed();
+  });
+
+  it("Must return the value if called retrieve.", async () => {
+    const val = await box.retrieve();
+    expect(val).to.eq(0);
   });
 
   it("Must emit ValueChanged if called store.", async () => {
@@ -23,7 +30,7 @@ describe("Box", () => {
     await expect(tx).to.emit(box, "ValueChanged").withArgs(42);
   });
 
-  it("Must return the value if called retrieve.", async () => {
+  it("Must return the value if called retrieve after changed.", async () => {
     const val = await box.retrieve();
     expect(val).to.eq(42);
   });
